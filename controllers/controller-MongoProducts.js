1348f98d33a852f5e815e0e6f0d9daf8e84fa153
unknown <almen.elias@gmail.com>
2022-12-06 22:06:36 +0100

    mongoDB 03

--- controllers/controller-MongoProducts.js
+++ controllers/controller-MongoProducts.js
@@ -6,25 +6,37 @@ const mongoProductSchema = require('../schemas/mongoProductSchema')
 
 // Unsecured routes
 controller.route('/')
-.get(async(req, res) =>{
-    try {
-        res.status(200).json(await mongoProductSchema.find())
-    } catch {
-        res.status(400).json()
-    }
-})
+    .get(async (req, res) => {
+        try {
+            res.status(200).json(await mongoProductSchema.find())
+        } catch {
+            res.status(400).json()
+        }
+    })
 controller.route('/:tag')
-.get((req, res) =>{
-
-})
+    .get(async (req, res) => {
+        const products = await mongoProductSchema.find({ tag: req.params.tag })
+        if (products)
+            res.status(200).json(products)
+        else
+            res.status(400).json()
+    })
 controller.route('/:tag/:take')
-.get((req, res) =>{
-
-})
-controller.route('/details/:articleNumber')
-.get((req, res) =>{
-
-})
+    .get(async (req, res) => {
+        const products = await mongoProductSchema.find({ tag: req.params.tag }).limit(req.params.take)
+        if (products)
+            res.status(200).json(products)
+        else
+            res.status(400).json()
+    })
+controller.route('/product/details/:articleNumber')
+    .get(async (req, res) => {
+        const product = await mongoProductSchema.findById(req.params.articleNumber)
+        if (product)
+            res.status(200).json(product)
+        else
+            res.status(404).json()
+    })
 
 // Secured routes
 
